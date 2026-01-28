771c33ad98054e1ea5a878cf587b98281a2d90e9
Iblal Ahmed <iblalahmed12@gmail.com>
2026-01-28 15:21:52 +0000

    added margin to patients header

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div className="m-2">
-        <h1 className="text-3xl">Patients</h1>
+        <h1 className="text-3xl mb-3">Patients</h1>
         <div className="flex flex-col gap-4">
           {patients.map((patient) => (
             <div
@@ -19,16 +19,16 @@ function App() {
               className="border border-gray-300 rounded-lg p-4 max-w-xs"
             >
               <div>
-              <strong>ID:</strong> {patient.id}
+                <strong>ID:</strong> {patient.id}
               </div>
               <div>
-              <strong>First Name:</strong> {patient.firstName}
+                <strong>First Name:</strong> {patient.firstName}
               </div>
               <div>
-              <strong>Last Name:</strong> {patient.lastName}
+                <strong>Last Name:</strong> {patient.lastName}
               </div>
               <div>
-              <strong>Age:</strong> {patient.age}
+                <strong>Age:</strong> {patient.age}
               </div>
             </div>
           ))}
