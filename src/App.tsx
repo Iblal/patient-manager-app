728f2b9d3397a970aa42d5e8885e28b1fc25aebe
Iblal Ahmed <iblalahmed12@gmail.com>
2026-01-28 15:20:17 +0000

    updated to use tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,23 @@ function App() {
     <>
       <div className="m-2">
         <h1 className="text-3xl">Patients</h1>
-        <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+        <div className="flex flex-col gap-4">
           {patients.map((patient) => (
             <div
               key={patient.id}
-              style={{
-                border: "1px solid #ccc",
-                borderRadius: "8px",
-                padding: "16px",
-                maxWidth: "300px",
-              }}
+              className="border border-gray-300 rounded-lg p-4 max-w-xs"
             >
               <div>
-                <strong>ID:</strong> {patient.id}
+              <strong>ID:</strong> {patient.id}
               </div>
               <div>
-                <strong>First Name:</strong> {patient.firstName}
+              <strong>First Name:</strong> {patient.firstName}
               </div>
               <div>
-                <strong>Last Name:</strong> {patient.lastName}
+              <strong>Last Name:</strong> {patient.lastName}
               </div>
               <div>
-                <strong>Age:</strong> {patient.age}
+              <strong>Age:</strong> {patient.age}
               </div>
             </div>
           ))}
