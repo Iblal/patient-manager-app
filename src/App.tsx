de7c934b0d125432cbf22b43d061a204d391e72b
Iblal Ahmed <iblalahmed12@gmail.com>
2026-01-28 15:04:56 +0000

    added hardcoded patients

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import "./App.css";
+import type { Patient } from "./types/patient";
+
+const patients: Patient[] = [
+  { id: "1", firstName: "John", lastName: "Doe", age: 35 },
+  { id: "2", firstName: "Jane", lastName: "Smith", age: 28 },
+  { id: "3", firstName: "Bob", lastName: "Johnson", age: 42 },
+];
 
 function App() {
   return (
